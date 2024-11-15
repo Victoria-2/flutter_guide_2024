7e50576f405f660766e0cdf795955f2750b3180d
sganan81 <sganan81@gmail.com>
2024-11-15 16:19:42 -0300

    update providers and helpers

--- lib/helpers/photos_future.dart
+++ lib/helpers/photos_future.dart
@@ -4,12 +4,13 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_guide_2024/models/lista_fotos_models.dart';
 
 class FotosFuture {
-  static Future<List<Photos>> getPhotos() async {
+  static Future<List<Photos>> getPhotos([int page = 1]) async {
     final url = Uri.https('api.unsplash.com', '/photos',
-        {'client_id': dotenv.env['API_KEY_UNSPLASH'], 'page': '1'});
+        {'client_id': dotenv.env['API_KEY_UNSPLASH'], 'page': '$page'});
+    print(url);
     final response = await http.get(url);
-    print(response.body);
     if (response.statusCode == 200) {
+      print('ok from mockapi');
       return photosFromJson(response.body);
     } else {
       throw Exception('Failed to load photos');
--- lib/providers/people_provider.dart
+++ lib/providers/people_provider.dart
@@ -1,28 +1,53 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_guide_2024/models/company_model.dart';
+import 'package:flutter_guide_2024/models/list_companies.dart';
 import 'package:flutter_guide_2024/models/list_people_model.dart';
+import 'package:flutter_guide_2024/models/people_model.dart';
 import 'package:http/http.dart' as http;
 
 class PeopleProvider extends ChangeNotifier {
-  List listPeople = [];
+  List<People> listPeople = [];
+  List<Company> listCompanies = [];
 
   PeopleProvider() {
     print('PeopleProvider......');
     this.getPeople();
+    this.getCompanies();
   }
 
-  getPeople() async {
+  getPeople([int page = 1, int limit = 10]) async {
     try {
-      print('realizando petición a MockAPI......');
-      final url =
-          Uri.https('66c78f59732bf1b79fa6e8c7.mockapi.io', '/api/v1/empleados');
+      print('realizando petición a MockAPI People......');
+      final url = Uri.https('66c78f59732bf1b79fa6e8c7.mockapi.io',
+          '/api/v1/empleados', {'page': '$page', 'limit': '$limit'});
+      print(url);
       final response = await http.get(url);
+      print('response ${response.statusCode}');
       if (response.statusCode == 200) {
         listPeople = peopleFromJson(response.body);
       } else {
-        print('4xx 5xx');
+        print('Error en el servicio: ${response.statusCode}');
       }
     } catch (e) {
-      print('Error $e');
+      print('Error al realizar el request: $e');
+    }
+    notifyListeners();
+  }
+
+  getCompanies([int page = 1, int limit = 10]) async {
+    try {
+      print('realizando petición a MockAPI companies......');
+      final url = Uri.https('66c78f59732bf1b79fa6e8c7.mockapi.io',
+          '/api/v1/companies', {'page': '$page', 'limit': '$limit'});
+      final response = await http.get(url);
+      print('response ${response.statusCode}');
+      if (response.statusCode == 200) {
+        listCompanies = companyFromJson(response.body);
+      } else {
+        print('Error en el servicio: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error al realizar el request: $e');
     }
     notifyListeners();
   }
